d44fd1b78c30d72c2255cf386d0341ce41fb3b1d
Robert Durfee <robert.durfee.17@gmail.com>
2019-02-02 09:07:19 -0600

    made gcs download function

--- sequence/utils.py
+++ sequence/utils.py
@@ -24,3 +24,25 @@ def copy_to_gcs(local_file, remote_file):
     blob = bucket.blob(object_name)
 
     blob.upload_from_filename(local_file)
+
+
+def copy_from_gcs(remote_file, local_file):
+    """Copy a file from remote Google Cloud Storage.
+
+    Args:
+        remote_file (str): Path to remote file source.
+        local_file (str): Path to local file destination.
+
+    """
+    match = gcs_url_re.match(remote_file)
+    if not match:
+        raise ValueError('\'' + remote_file + '\' is a malformed GCS URL.')
+
+    bucket_name = match['bucket_name']
+    object_name = match['object_name'].replace('\\', '/')
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(object_name)
+
+    blob.download_to_filename(local_file)
